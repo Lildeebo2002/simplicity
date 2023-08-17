Lildeebo2002/simplicity

commit df19a4ddb3d0dcc8acd12418343a9eec91a8b554

Return error codes upon failure

C/rsort.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include <simplicity/errorCodes.h>
 #include "limitations.h"
 #include "sha256.h"
 #include "simplicity_assert.h"
@@ -17,18 +18,15 @@ _Static_assert(UCHAR_MAX < SIZE_MAX, "UCHAR_MAX >= SIZE_MAX");
 const sha256_midstate* rsort(size_t* scratch, const sha256_midstate** a, size_t len, size_t level);
 
 /* Searches for duplicates in an array of 'sha256_midstate's.
- * If 'true' is returned, '*duplicates' is set according to whether duplicate 'sha256_midstate' values were found.
- * If malloc fails, false is returned and '*duplicates' is unchanged.
+ * If malloc fails, returns -1.
+ * If no duplicates are found, returns 0.
+ * If duplicates are found, returns a positive value.
  *
- * Precondition: NULL != duplicates;
- *               const sha256_midstate a[len];
+ * Precondition: const sha256_midstate a[len];
  *               len <= DAG_LEN_MAX;
  */
-static inline bool hasDuplicates(bool* duplicates, const sha256_midstate* a, size_t len) {
-  if (0 == len) {
-    *duplicates = false;
-    return true;
-  }
+static inline int hasDuplicates(const sha256_midstate* a, size_t len) {
+  if (0 == len) return SIMPLICITY_NO_ERROR;
   static_assert(sizeof(a->s) * CHAR_BIT == 256, "sha256_midstate.s has unnamed padding.");
   static_assert(sizeof(a->s) < SIZE_MAX / CHAR_COUNT, "CHAR_BIT is way too large.");
   static_assert((sizeof(a->s) + 1) * CHAR_COUNT <= SIZE_MAX/sizeof(size_t), "sizeof(size_t) is way too large.");
@@ -39,14 +37,14 @@ static inline bool hasDuplicates(bool* duplicates, const sha256_midstate* a, siz
   simplicity_assert(len <= SIZE_MAX / sizeof(const sha256_midstate*));
   simplicity_assert(len - 1 <= UINT32_MAX);
   const sha256_midstate **perm = malloc(len * sizeof(const sha256_midstate*));
-  bool result = scratch && perm;
+  int result = scratch && perm ? 0 : -1;
 
-  if (result) {
+  if (0 <= result) {
     for (size_t i = 0; i < len; ++i) {
       perm[i] = a + i;
     }
 
-    *duplicates = rsort(scratch, perm, len, sizeof(a->s));
+    result = NULL != rsort(scratch, perm, len, sizeof(a->s));
   }
 
   free(perm);

C/bitstream.c
@@ -5,19 +5,23 @@
 #include "simplicity_assert.h"
 
 /* Closes a bitstream by consuming all remaining bits.
- * Returns false if CHAR_BIT or more bits remain in the stream or if any remaining bits are non-zero.
+ * Returns 'SIMPLICITY_ERR_BITSTREAM_UNUSED_BYTES' if CHAR_BIT or more bits remain in the stream.
+ * Otherwise, returns 'SIMPLICITY_ERR_BITSTREAM_UNUSED_BITS' if any remaining bits are non-zero.
+ * Otherwise returns 'SIMPLICITY_NO_ERROR'.
  *
  * Precondition: NULL != stream
  */
-bool closeBitstream(bitstream* stream) {
-  if (1 < stream->len) return false; /* If there is more than one byte remaining. */
-  if (1 == stream->len &&                                  /* If there is one byte remaining */
-      (0 == stream->offset ||                              /* and either no bits have been consumed */
-       0 != (*stream->arr & (UCHAR_MAX >> stream->offset)) /* or any of the unconsumed bits are non-zero */
-     )) return false;
+simplicity_err closeBitstream(bitstream* stream) {
+  if (1 < stream->len) return SIMPLICITY_ERR_BITSTREAM_UNUSED_BYTES;        /* If there is more than one byte remaining. */
+  if (1 == stream->len) {
+    if (0 == stream->offset) return SIMPLICITY_ERR_BITSTREAM_UNUSED_BYTES;  /* If there is one byte remaining */
+    if (0 != (*stream->arr & (UCHAR_MAX >> stream->offset))) {              /* If any of the unconsumed bits are non-zero */
+      return SIMPLICITY_ERR_BITSTREAM_UNUSED_BITS;
+    }
+  }
   /* Otherwise there are either 0 bits remaining or there are between 1 and CHAR_BITS-1 bits remaining and they are all zero. */
   *stream = (bitstream){0};
-  return true;
+  return SIMPLICITY_NO_ERROR;
 }
 
 /* Fetches up to 31 bits from 'stream' as the 'n' least significant bits of return value.
@@ -200,7 +204,7 @@ int32_t decodeUptoMaxInt(bitstream* stream) {
 
 /* Fills a 'bitstring' containing 'n' bits from 'stream'.
  * Returns 'SIMPLICITY_ERR_BITSTREAM_EOF' if not enough bits are available.
- * If successful, '*result' is set to a bitstring with 'n' bits read from 'stream' and 0 is returned.
+ * If successful, '*result' is set to a bitstring with 'n' bits read from 'stream' and 'SIMPLICITY_NO_ERROR' is returned.
  *
  * If an error is returned '*result' might be modified.
  *
@@ -208,7 +212,7 @@ int32_t decodeUptoMaxInt(bitstream* stream) {
  *               n <= 2^31
  *               NULL != stream
  */
-int32_t readBitstring(bitstring* result, size_t n, bitstream* stream) {
+simplicity_err readBitstring(bitstring* result, size_t n, bitstream* stream) {
   static_assert(0x8000u + 2*(CHAR_BIT - 1) <= SIZE_MAX, "size_t needs to be at least 32-bits");
   simplicity_assert(n <= 0x8000u);
   size_t total_offset = n + stream->offset;
@@ -226,5 +230,5 @@ int32_t readBitstring(bitstring* result, size_t n, bitstream* stream) {
     stream->offset = total_offset % CHAR_BIT;
     /* Note that if 0 == stream->len then 0 == stream->offset. */
   }
-  return 0;
+  return SIMPLICITY_NO_ERROR;
 }

C/bitstream.h
@@ -30,11 +30,13 @@ static inline bitstream initializeBitstream(const unsigned char* arr, size_t len
 }
 
 /* Closes a bitstream by consuming all remaining bits.
- * Returns false if CHAR_BIT or more bits remain in the stream or if any remaining bits are non-zero.
+ * Returns 'SIMPLICITY_ERR_BITSTREAM_UNUSED_BYTES' if CHAR_BIT or more bits remain in the stream.
+ * Otherwise, returns 'SIMPLICITY_ERR_BITSTREAM_UNUSED_BITS' if any remaining bits are non-zero.
+ * Otherwise returns 'SIMPLICITY_NO_ERROR'.
  *
  * Precondition: NULL != stream
  */
-bool closeBitstream(bitstream* stream);
+simplicity_err closeBitstream(bitstream* stream);
 
 /* Fetches up to 31 bits from 'stream' as the 'n' least significant bits of return value.
  * The 'n' bits are set from the MSB to the LSB.
@@ -66,7 +68,7 @@ int32_t decodeUptoMaxInt(bitstream* stream);
 
 /* Fills a 'bitstring' containing 'n' bits from 'stream'.
  * Returns 'SIMPLICITY_ERR_BITSTREAM_EOF' if not enough bits are available.
- * If successful, '*result' is set to a bitstring with 'n' bits read from 'stream' and 0 is returned.
+ * If successful, '*result' is set to a bitstring with 'n' bits read from 'stream' and 'SIMPLICITY_NO_ERROR' is returned.
  *
  * If an error is returned '*result' might be modified.
  *
@@ -74,5 +76,5 @@ int32_t decodeUptoMaxInt(bitstream* stream);
  *               n <= 2^31
  *               NULL != stream
  */
-int32_t readBitstring(bitstring* result, size_t n, bitstream* stream);
+simplicity_err readBitstring(bitstring* result, size_t n, bitstream* stream);
 #endif

C/dag.c
@@ -360,20 +360,20 @@ void computeAnnotatedMerkleRoot(analyses* analysis, const dag_node* dag, const t
  * right branches, with the exception that nodes under right braches may (cross-)reference identical nodes that already occur under
  * left branches.
  *
- * Returns 'true' if the 'dag' is in canonical order, and returns 'false' if it is not.
+ * Returns 'SIMPLICITY_NO_ERROR' if the 'dag' is in canonical order, and returns 'SIMPLICITY_ERR_DATA_OUT_OF_ORDER' if it is not.
  *
  * May modify dag[i].aux values and invalidate dag[i].sourceType and dag[i].targetType.
  * This function should only be used prior to calling 'mallocTypeInference'.
  *
  * Precondition: dag_node dag[len] and 'dag' is well-formed.
  */
-bool verifyCanonicalOrder(dag_node* dag, const size_t len) {
+simplicity_err verifyCanonicalOrder(dag_node* dag, const size_t len) {
   size_t bottom = 0;
   size_t top = len-1; /* Underflow is checked below. */
 
   if (!len) {
     simplicity_assert(false); /* A well-formed dag has non-zero length */
-    return true; /* However, an empty dag is technically in canonical order */
+    return SIMPLICITY_NO_ERROR; /* However, an empty dag is technically in canonical order */
   }
 
   /* We use dag[i].aux as a "stack" to manage the traversal of the DAG. */
@@ -448,20 +448,22 @@ bool verifyCanonicalOrder(dag_node* dag, const size_t len) {
     }
 
     /* Check current node. */
-    if (bottom < top) return false; /* Not in canonical order.  */
+    if (bottom < top) return SIMPLICITY_ERR_DATA_OUT_OF_ORDER;
     if (bottom == top) bottom++;
     /* top < bottom */
     top = dag[top].aux; /* Return. */
   }
   simplicity_assert(bottom == top && top == len);
 
-  return true;
+  return SIMPLICITY_NO_ERROR;
 }
 
 /* This function fills in the 'WITNESS' nodes of a 'dag' with the data from 'witness'.
  * For each 'WITNESS' : A |- B expression in 'dag', the bits from the 'witness' bitstring are decoded in turn
  * to construct a compact representation of a witness value of type B.
- * This function only returns 'true' when exactly 'witness.len' bits are consumed by all the 'dag's witness values.
+ * This function only returns 'SIMPLICITY_NO_ERROR' when exactly 'witness.len' bits are consumed by all the 'dag's witness values.
+ * If extra bits remain, then 'SIMPLICITY_ERR_WITNESS_UNUSED_BITS' is returned.
+ * If there are not enough bits, then 'SIMPLICITY_ERR_WITNESS_EOF' is returned.
  *
  * Note: the 'witness' value is passed by copy because the implementation manipulates a local copy of the structure.
  *
@@ -469,16 +471,16 @@ bool verifyCanonicalOrder(dag_node* dag, const size_t len) {
  *               witness is a valid bitstring;
  *
  * Postcondition: dag_node dag[len] and 'dag' has witness data and is well-typed with 'type_dag'
- *                  when the result is 'true';
+ *                  when the result is 'SIMPLICITY_NO_ERROR';
  */
-bool fillWitnessData(dag_node* dag, type* type_dag, const size_t len, bitstring witness) {
+simplicity_err fillWitnessData(dag_node* dag, type* type_dag, const size_t len, bitstring witness) {
   for (size_t i = 0; i < len; ++i) {
     if (WITNESS == dag[i].tag) {
       if (witness.len <= 0) {
         /* There is no more data left in witness. */
         dag[i].compactValue = (bitstring){0};
         /* This is fine as long as the witness type is trivial */
-        if (type_dag[WITNESS_B(dag, type_dag, i)].bitSize) return false;
+        if (type_dag[WITNESS_B(dag, type_dag, i)].bitSize) return SIMPLICITY_ERR_WITNESS_EOF;
       } else {
         dag[i].compactValue = (bitstring)
           { .arr = &witness.arr[witness.offset/CHAR_BIT]
@@ -494,7 +496,7 @@ bool fillWitnessData(dag_node* dag, type* type_dag, const size_t len, bitstring 
           if (SUM == type_dag[cur].kind) {
             /* Parse one bit and traverse the left type or the right type depending on the value of the bit parsed. */
             simplicity_assert(calling);
-            if (witness.len <= 0) return false;
+            if (witness.len <= 0) return SIMPLICITY_ERR_WITNESS_EOF;
             bool bit = 1 & (witness.arr[witness.offset/CHAR_BIT] >> (CHAR_BIT - 1 - witness.offset % CHAR_BIT));
             witness.offset++; witness.len--;
             size_t next = typeSkip(type_dag[cur].typeArg[bit], type_dag);
@@ -545,28 +547,26 @@ bool fillWitnessData(dag_node* dag, type* type_dag, const size_t len, bitstring 
       }
     }
   }
-  return (0 == witness.len);
+  return 0 == witness.len ? SIMPLICITY_NO_ERROR : SIMPLICITY_ERR_WITNESS_UNUSED_BITS;
 }
 
 /* Computes the identity Merkle roots of every subexpression in a well-typed 'dag' with witnesses.
  * imr[i]' is set to the identity Merkle root of the subexpression 'dag[i]'.
  * When 'HIDDEN == dag[i].tag', then 'imr[i]' is instead set to a hidden root hash for that hidden node.
  *
- * If malloc fails, return 'false', otherwise return 'true'.
- * If 'true' is returned then '*success' is set to true if all the identity Merkle roots (and hidden roots) are all unique.
+ * If malloc fails, returns 'SIMPLICITY_ERR_MALLOC'.
+ * If all the identity Merkle roots (and hidden roots) are all unique, returns 'SIMPLICITY_NO_ERROR'.
+ * Otherwise returns 'SIMPLICITY_ERR_UNSHARED_SUBEXPRESSION'.
  *
- * Precondition: NULL != success;
- *               sha256_midstate imr[len];
+ * Precondition: sha256_midstate imr[len];
  *               dag_node dag[len] and 'dag' is well-typed with 'type_dag' and contains witnesses.
  */
-bool verifyNoDuplicateIdentityRoots(bool* success, sha256_midstate* imr,
-                                    const dag_node* dag, const type* type_dag, const size_t len) {
-  bool result, duplicates;
-
+simplicity_err verifyNoDuplicateIdentityRoots(sha256_midstate* imr, const dag_node* dag, const type* type_dag, const size_t len) {
   computeIdentityMerkleRoot(imr, dag, type_dag, len);
 
-  result = hasDuplicates(&duplicates, imr, len);
-  *success = result && !duplicates;
-
-  return result;
+  switch (hasDuplicates(imr, len)) {
+  case -1: return SIMPLICITY_ERR_MALLOC;
+  case 0: return SIMPLICITY_NO_ERROR;
+  default: return SIMPLICITY_ERR_UNSHARED_SUBEXPRESSION;
+  }
 }

C/dag.h
@@ -6,6 +6,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <simplicity/errorCodes.h>
 #include "bitstring.h"
 #include "bounded.h"
 #include "jets.h"
@@ -357,40 +358,41 @@ void computeAnnotatedMerkleRoot(analyses* analysis, const dag_node* dag, const t
  * right branches, with the exception that nodes under right braches may (cross-)reference identical nodes that already occur under
  * left branches.
  *
- * Returns 'true' if the 'dag' is in canonical order, and returns 'false' if it is not.
+ * Returns 'SIMPLICITY_NO_ERROR' if the 'dag' is in canonical order, and returns 'SIMPLICITY_ERR_DATA_OUT_OF_ORDER' if it is not.
  *
  * May modify dag[i].aux values and invalidate dag[i].sourceType and dag[i].targetType.
  * This function should only be used prior to calling 'mallocTypeInference'.
  *
  * Precondition: dag_node dag[len] and 'dag' is well-formed.
  */
-bool verifyCanonicalOrder(dag_node* dag, const size_t len);
+simplicity_err verifyCanonicalOrder(dag_node* dag, const size_t len);
 
 /* This function fills in the 'WITNESS' nodes of a 'dag' with the data from 'witness'.
  * For each 'WITNESS' : A |- B expression in 'dag', the bits from the 'witness' bitstring are decoded in turn
  * to construct a compact representation of a witness value of type B.
- * This function only returns 'true' when exactly 'witness.len' bits are consumed by all the 'dag's witness values.
+ * This function only returns 'SIMPLICITY_NO_ERROR' when exactly 'witness.len' bits are consumed by all the 'dag's witness values.
+ * If extra bits remain, then 'SIMPLICITY_ERR_WITNESS_UNUSED_BITS' is returned.
+ * If there are not enough bits, then 'SIMPLICITY_ERR_WITNESS_EOF' is returned.
  *
  * Precondition: dag_node dag[len] and 'dag' without witness data and is well-typed with 'type_dag';
  *               witness is a valid bitstring;
  *
  * Postcondition: dag_node dag[len] and 'dag' has witness data and is well-typed with 'type_dag'
- *                  when the result is 'true';
+ *                  when the result is 'SIMPLICITY_NO_ERROR';
  */
-bool fillWitnessData(dag_node* dag, type* type_dag, const size_t len, bitstring witness);
+simplicity_err fillWitnessData(dag_node* dag, type* type_dag, const size_t len, bitstring witness);
 
 /* Computes the identity Merkle roots of every subexpression in a well-typed 'dag' with witnesses.
  * imr[i]' is set to the identity Merkle root of the subexpression 'dag[i]'.
  * When 'HIDDEN == dag[i].tag', then 'imr[i]' is instead set to a hidden root hash for that hidden node.
  *
- * If malloc fails, return 'false', otherwise return 'true'.
- * If 'true' is returned then '*success' is set to true if all the identity Merkle roots (and hidden roots) are all unique.
+ * If malloc fails, returns 'SIMPLICITY_ERR_MALLOC'.
+ * If all the identity Merkle roots (and hidden roots) are all unique, returns 'SIMPLICITY_NO_ERROR'.
+ * Otherwise returns 'SIMPLICITY_ERR_UNSHARED_SUBEXPRESSION'.
  *
- * Precondition: NULL != success;
- *               sha256_midstate imr[len];
+ * Precondition: sha256_midstate imr[len];
  *               dag_node dag[len] and 'dag' is well-typed with 'type_dag' and contains witnesses.
  */
-bool verifyNoDuplicateIdentityRoots(bool* success, sha256_midstate* imr,
-                                    const dag_node* dag, const type* type_dag, const size_t len);
+simplicity_err verifyNoDuplicateIdentityRoots(sha256_midstate* imr, const dag_node* dag, const type* type_dag, const size_t len);
 
 #endif

C/deserialize.c
@@ -9,32 +9,32 @@
 /* Fetches 'len' 'uint32_t's from 'stream' into 'result'.
  * The bits in each 'uint32_t' are set from the MSB to the LSB and the 'uint32_t's of 'result' are set from 0 up to 'len'.
  * Returns 'SIMPLICITY_ERR_BITSTREAM_EOF' if not enough bits are available ('result' may be modified).
- * Returns 0 if successful.
+ * Returns 'SIMPLICITY_NO_ERROR' if successful.
  *
  * Precondition: uint32_t result[len];
  *               NULL != stream
  */
-static int32_t getWord32Array(uint32_t* result, const size_t len, bitstream* stream) {
+static simplicity_err getWord32Array(uint32_t* result, const size_t len, bitstream* stream) {
   for (size_t i = 0; i < len; ++i) {
     /* Due to error codes, readNBits cannot fetch 32 bits at once. Instead we fetch two groups of 16 bits. */
     int32_t bits16 = readNBits(16, stream);
-    if (bits16 < 0) return bits16;
+    if (bits16 < 0) return (simplicity_err)bits16;
     result[i] = (uint32_t)bits16 << 16;
     bits16 = readNBits(16, stream);
-    if (bits16 < 0) return bits16;
+    if (bits16 < 0) return (simplicity_err)bits16;
     result[i] |= (uint32_t)bits16;
   }
-  return 0;
+  return SIMPLICITY_NO_ERROR;
 }
 
 /* Fetches a 256-bit hash value from 'stream' into 'result'.
  * Returns 'SIMPLICITY_ERR_BITSTREAM_EOF' if not enough bits are available ('result' may be modified).
- * Returns 0 if successful.
+ * Returns 'SIMPLICITY_NO_ERROR' if successful.
  *
  * Precondition: NULL != result
  *               NULL != stream
  */
-static int32_t getHash(sha256_midstate* result, bitstream* stream) {
+static simplicity_err getHash(sha256_midstate* result, bitstream* stream) {
   return getWord32Array(result->s, 8, stream);
 }
 
@@ -49,29 +49,29 @@ static int32_t getHash(sha256_midstate* result, bitstream* stream) {
  *                                            or the size of a WORD encoding is greater than 2^31 bits.
  * Returns 'SIMPLICITY_ERR_BITSTRING_EOF' if not enough bits are available in the 'stream'.
  * In the above error cases, 'dag' may be modified.
- * Returns 0 if successful.
+ * Returns 'SIMPLICITY_NO_ERROR' if successful.
  *
  * Precondition: dag_node dag[i + 1];
  *               i < 2^31 - 1
  *               NULL != stream
  */
-static int32_t decodeNode(dag_node* dag, size_t i, bitstream* stream) {
+static simplicity_err decodeNode(dag_node* dag, size_t i, bitstream* stream) {
   int32_t bit = read1Bit(stream);
-  if (bit < 0) return bit;
+  if (bit < 0) return (simplicity_err)bit;
   dag[i] = (dag_node){0};
   if (bit) {
     bit = read1Bit(stream);
-    if (bit < 0) return bit;
+    if (bit < 0) return (simplicity_err)bit;
     if (bit) {
       return decodeJet(&dag[i], stream);
     } else {
       /* Decode WORD. */
       int32_t depth = decodeUptoMaxInt(stream);
-      if (depth < 0) return depth;
+      if (depth < 0) return (simplicity_err)depth;
       if (32 < depth) return SIMPLICITY_ERR_DATA_OUT_OF_RANGE;
       {
-        int32_t result = readBitstring(&dag[i].compactValue, (size_t)1 << (depth - 1), stream);
-        if (result < 0) return result;
+        simplicity_err error = readBitstring(&dag[i].compactValue, (size_t)1 << (depth - 1), stream);
+        if (0 != error) return error;
       }
       dag[i].tag = WORD;
       dag[i].targetIx = (size_t)depth;
@@ -79,12 +79,12 @@ static int32_t decodeNode(dag_node* dag, size_t i, bitstream* stream) {
     }
   } else {
     int32_t code = readNBits(2, stream);
-    if (code < 0) return code;
+    if (code < 0) return (simplicity_err)code;
     int32_t subcode = readNBits(code < 3 ? 2 : 1, stream);
-    if (subcode < 0) return subcode;
+    if (subcode < 0) return (simplicity_err)subcode;
     for (int32_t j = 0; j < 2 - code; ++j) {
       int32_t ix = decodeUptoMaxInt(stream);
-      if (ix < 0) return ix;
+      if (ix < 0) return (simplicity_err)ix;
       if (i < (uint32_t)ix) return SIMPLICITY_ERR_DATA_OUT_OF_RANGE;
       dag[i].child[j] = i - (uint32_t)ix;
     }
@@ -136,7 +136,7 @@ static int32_t decodeNode(dag_node* dag, size_t i, bitstream* stream) {
 
     computeCommitmentMerkleRoot(dag, i);
   }
-  return 0;
+  return SIMPLICITY_NO_ERROR;
 }
 
 /* Decode a Simplicity DAG consisting of 'len' nodes from 'stream' into 'dag'.
@@ -147,20 +147,20 @@ static int32_t decodeNode(dag_node* dag, size_t i, bitstream* stream) {
  * Returns 'SIMPLICITY_ERR_HIDDEN' if there are illegal HIDDEN children in the DAG.
  * Returns 'SIMPLICITY_ERR_BITSTRING_EOF' if not enough bits are available in the 'stream'.
  * In the above error cases, 'dag' may be modified.
- * Returns 0 if successful.
+ * Returns 'SIMPLICITY_NO_ERROR' if successful.
  *
  * Precondition: dag_node dag[len];
  *               len < 2^31
  *               NULL != stream
  */
-static int32_t decodeDag(dag_node* dag, const size_t len, combinator_counters* census, bitstream* stream) {
+static simplicity_err decodeDag(dag_node* dag, const size_t len, combinator_counters* census, bitstream* stream) {
   for (size_t i = 0; i < len; ++i) {
-    int32_t err = decodeNode(dag, i, stream);
-    if (err < 0) return err;
+    simplicity_err error = decodeNode(dag, i, stream);
+    if (0 != error) return error;
 
     enumerator(census, dag[i].tag);
   }
-  return 0;
+  return SIMPLICITY_NO_ERROR;
 }
 
 /* Decode a length-prefixed Simplicity DAG from 'stream'.
@@ -171,6 +171,7 @@ static int32_t decodeDag(dag_node* dag, const size_t len, combinator_counters* c
  * Returns 'SIMPLICITY_ERR_STOP_CODE' if the encoding of a stop tag is encountered.
  * Returns 'SIMPLICITY_ERR_HIDDEN' if there are illegal HIDDEN children in the DAG.
  * Returns 'SIMPLICITY_ERR_BITSTRING_EOF' if not enough bits are available in the 'stream'.
+ * Returns 'SIMPLICITY_ERR_DATA_OUT_OF_ORDER' if nodes are not serialized in the canonical order.
  * Returns 'SIMPLICITY_ERR_MALLOC' if malloc fails.
  * In the above error cases, '*dag' is set to NULL.
  * If successful, returns a positive value equal to the length of an allocated array of (*dag).
@@ -197,16 +198,16 @@ int32_t decodeMallocDag(dag_node** dag, combinator_counters* census, bitstream* 
   if (!*dag) return SIMPLICITY_ERR_MALLOC;
 
   if (census) *census = (combinator_counters){0};
-  int32_t err = decodeDag(*dag, (size_t)dagLen, census, stream);
+  simplicity_err error = decodeDag(*dag, (size_t)dagLen, census, stream);
 
-  if (0 <= err && !verifyCanonicalOrder(*dag, (size_t)(dagLen))) {
-    err = SIMPLICITY_ERR_DATA_OUT_OF_ORDER;
+  if (0 == error) {
+    error = verifyCanonicalOrder(*dag, (size_t)(dagLen));
   }
 
-  if (err < 0) {
+  if (0 != error) {
     free(*dag);
     *dag = NULL;
-    return err;
+    return (int32_t)error;
   } else {
     return dagLen;
   }
@@ -217,18 +218,18 @@ int32_t decodeMallocDag(dag_node** dag, combinator_counters* census, bitstream* 
  * Returns 'SIMPLICITY_ERR_DATA_OUT_OF_RANGE' if the encoded string of bits exceeds this decoder's limits.
  * Returns 'SIMPLICITY_ERR_BITSTRING_EOF' if not enough bits are available in the 'stream'.
  * If successful, '*witness' is set to the decoded bitstring,
- *                and 0 is returned.
+ *                and 'SIMPLICITY_NO_ERROR' is returned.
  *
  * If an error is returned '*witness' might be modified.
  *
  * Precondition: NULL != witness;
  *               NULL != stream;
  */
-int32_t decodeWitnessData(bitstring* witness, bitstream* stream) {
+simplicity_err decodeWitnessData(bitstring* witness, bitstream* stream) {
   int32_t witnessLen = read1Bit(stream);
-  if (witnessLen < 0) return witnessLen;
+  if (witnessLen < 0) return (simplicity_err)witnessLen;
   if (0 < witnessLen) witnessLen = decodeUptoMaxInt(stream);
-  if (witnessLen < 0) return witnessLen;
+  if (witnessLen < 0) return (simplicity_err)witnessLen;
 
   return readBitstring(witness, (size_t)witnessLen, stream);
 }

C/deserialize.h
@@ -34,13 +34,13 @@ int32_t decodeMallocDag(dag_node** dag, combinator_counters* census, bitstream* 
  * Returns 'SIMPLICITY_ERR_DATA_OUT_OF_RANGE' if the encoded string of bits exceeds this decoder's limits.
  * Returns 'SIMPLICITY_ERR_BITSTRING_EOF' if not enough bits are available in the 'stream'.
  * If successful, '*witness' is set to the decoded bitstring,
- *                and 0 is returned.
+ *                and 'SIMPLICITY_NO_ERR' is returned.
  *
  * If an error is returned '*witness' might be modified.
  *
  * Precondition: NULL != witness;
  *               NULL != stream;
  */
-int32_t decodeWitnessData(bitstring* witness, bitstream* stream);
+simplicity_err decodeWitnessData(bitstring* witness, bitstream* stream);
 
 #endif

commit 6873ff5bbf0d41da6ddbc798f3bab3e78d5e3b06

No Simplicity Errors for rsort
Simplicity errors were unintentionally introduced in
df19a4ddb3d0dcc8acd12418343a9eec91a8b554.
However the specification defines integer error codes, not Simplicity ones.

C/rsort.h
@@ -6,7 +6,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#include <simplicity/errorCodes.h>
 #include "limitations.h"
 #include "sha256.h"
 #include "simplicity_assert.h"
@@ -26,7 +25,7 @@ const sha256_midstate* rsort(size_t* scratch, const sha256_midstate** a, size_t 
  *               len <= DAG_LEN_MAX;
  */
 static inline int hasDuplicates(const sha256_midstate* a, size_t len) {
-  if (0 == len) return SIMPLICITY_NO_ERROR;
+  if (0 == len) return 0;
   static_assert(sizeof(a->s) * CHAR_BIT == 256, "sha256_midstate.s has unnamed padding.");
   static_assert(sizeof(a->s) < SIZE_MAX / CHAR_COUNT, "CHAR_BIT is way too large.");
   static_assert((sizeof(a->s) + 1) * CHAR_COUNT <= SIZE_MAX/sizeof(size_t), "sizeof(size_t) is way too large.");